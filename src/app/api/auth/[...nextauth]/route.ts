86ea424d24b70618b17ed09e36815b127c6d843e
Goal2 <49075144+Goal2@users.noreply.github.com>
2025-08-31 05:04:33 +0200

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 // src/app/api/auth/[...nextauth]/route.ts
 import NextAuth from "next-auth";
-import { authOptions } from "@/auth"; // ou chemin relatif: "../../../../auth"
+import { authOptions } from "@/auth";
 
 const handler = NextAuth(authOptions);
 
